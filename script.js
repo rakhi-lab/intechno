7a53317fe09c25e9f3cd758898ed78456ff8716f
Rakhi Agrahari <rakhi158agrahari@gmail.com>
2026-01-02 17:58:34 +0530

    intechno home page

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ hamburger.addEventListener("click", () => {
       $(this).removeClass("hover");
     }
   );
+
+  // service section js
+  /* ===== Scroll Reveal ===== */
+
+document.querySelectorAll(".wow-card").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateX = ((y / rect.height) - 0.5) * 12;
+    const rotateY = ((x / rect.width) - 0.5) * -12;
+
+    card.style.transform = `
+      rotateX(${rotateX}deg)
+      rotateY(${rotateY}deg)
+      translateY(-10px)
+    `;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0) translateY(0)";
+  });
+});
+
